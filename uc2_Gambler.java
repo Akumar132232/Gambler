c26f0d2dded476e6930c29632b981fa3cd2bf351
Ankit Kumar <briantorrocta@gmail.com>
2022-07-06 00:35:23 +0530

    uc2_win_or_loose

---
+++ uc2_Gambler.java
@@ -0,0 +1,27 @@
+package Gambler_Problem.src.uc2_win_or_loose;
+
+public class uc2_Gambler {
+    public static final int STAKE_PER_DAY = 100;
+    public static final int BET = 1;
+    public static final int WIN = 1;
+    public static final int LOOSE = 0;
+
+    public static void dailyBet() {
+        int cash = STAKE_PER_DAY;
+        int option = (int) (Math.random() * 10 % 2);
+        if (option == WIN) {
+            cash += BET;
+            System.out.println("Player has won " + cash);
+        } else if (option == LOOSE) {
+            cash -= BET;
+
+            System.out.println("Player has loose " + cash);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome To Gambling Simulation");
+        dailyBet();
+
+    }
+}
