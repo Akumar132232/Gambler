453478a6989e672487edc92c5465cfdecdc56343
Ankit Kumar <briantorrocta@gmail.com>
2022-07-06 00:17:05 +0530

    uc1_stake&bet

---
+++ uc1_Gambler.java
@@ -0,0 +1,9 @@
+class uc1_Gambler{
+
+    public static void main(String[] args) {
+        System.out.println("Welcome To Gambling Simulation");
+        final int STAKE_PER_DAY = 100;
+        final int BET = 1;
+    }
+}
+
