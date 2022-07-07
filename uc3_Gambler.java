4d9411713f524091c44988a7f7844979c2996863
Akumar132232 <64557746+Akumar132232@users.noreply.github.com>
2022-07-07 14:28:22 +0530

    Create uc3_Gambler.java
    uc3_stakeResign

---
+++ uc3_Gambler.java
@@ -0,0 +1,33 @@
+package Gambler_Problem.src.uc3_stakeResign;
+
+public class uc3_Gambler {
+    public static final int STAKE_PER_DAY = 100;
+    public static final int BET = 1;
+    public static final int WIN = 1;
+    public static final int LOOSE = 0;
+
+    // Function to Check Win Or Loss
+    public static void dailyBet() {
+        int min_Percent = STAKE_PER_DAY - (STAKE_PER_DAY * 50 / 100);
+        int mxm_Percent = STAKE_PER_DAY + (STAKE_PER_DAY * 50 / 100);
+        int cash = STAKE_PER_DAY;
+        int option = (int) (Math.random() * 10 % 2);
+        while (cash > min_Percent && cash < mxm_Percent) {
+            if (option == WIN) {
+                cash += BET;
+                System.out.println("Player has won " + cash);
+            } else if (option == LOOSE) {
+                cash -= BET;
+
+                System.out.println("Player has loose " + cash);
+            }
+        }
+        System.out.println("Player has left with Total amount  " + cash);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome To Gambling Simulation");
+        dailyBet();
+
+    }
+}
